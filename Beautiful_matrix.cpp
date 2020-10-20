38c3b4f2ced4b4796070b421a964d59d0e46e3d0
iamgope <amangope005@gmail.com>
2020-10-20 10:18:10 +0530

    added few solutions

---
+++ Beautiful_matrix.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+     #define ll long long int
+    #define fi first
+    #define se second
+    #define m_p make_pair
+    #define endl '\n'
+    #define fast_io ios_base::sync_with_stdio(0); cin.tie(0)
+     #define endl '\n'
+#define loop(i, a, b) for (long long unsigned int i = a; i < b; i++)
+    using namespace std;
+     
+   // typedef long long ll;
+     
+    const int MAXN = 412345;
+    const int MAXINT = 2047483098;
+    const ll MOD = 1e9 + 7;
+    const int MAX = 1e4;
+     
+    const long double EPS = 1e-10;
+     int gcd(int a,int b){if(b==0)return a;return gcd(b,a%b);}
+
+bool isPrime(long long unsigned n){if(n<=1)return false;if(n<=3)return true;
+	if(n%2==0||n%3==0)return false;for(int i=5;i*i<=n;i=i+6)
+	if(n%i==0||n%(i+2)==0)return false;return true;}
+
+int nCr(int n,int r){int res=1;if(n<r)return -1;r=min(r,n-r);
+	loop(i,0,r){res*=(n-i);res/=(i+1);}return res;}
+
+int nPr(int n,int r){int res=1;if(n<r)return -1;
+	loop(i,0,r)res*=(n-i);return res;}
+   
+     
+    void solve() {
+     int arr[5][5];
+     int a=0,b=0;
+     for(int i=0;i<5;i++){
+     	for(int j=0;j<5;j++){
+     		cin>>arr[i][j];
+     		if(arr[i][j]==1)
+     		{
+     			a=i;
+     			b=j;
+		     }
+	     }
+     }
+     cout<<abs(2-a)+abs(2-b)<<endl;
+     
+     
+    
+}
+    int main()
+    {
+        fast_io;
+        
+        int T;
+        //cin >> T;
+      T=1;
+        while(T--) {
+            solve();
+        }
+       
+        return 0;
+    }
---
+++ Queue_at_school.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+
+     #define ll long long int
+    #define fi first
+    #define se second
+    #define m_p make_pair
+    #define endl '\n'
+    #define fast_io ios_base::sync_with_stdio(0); cin.tie(0)
+     #define endl '\n'
+#define loop(i, a, b) for (long long unsigned int i = a; i < b; i++)
+    using namespace std;
+     
+   // typedef long long ll;
+     
+    const int MAXN = 412345;
+    const int MAXINT = 2047483098;
+    const ll MOD = 1e9 + 7;
+    const int MAX = 1e4;
+     
+    const long double EPS = 1e-10;
+     int gcd(int a,int b){if(b==0)return a;return gcd(b,a%b);}
+
+bool isPrime(long long unsigned n){if(n<=1)return false;if(n<=3)return true;
+	if(n%2==0||n%3==0)return false;for(int i=5;i*i<=n;i=i+6)
+	if(n%i==0||n%(i+2)==0)return false;return true;}
+
+int nCr(int n,int r){int res=1;if(n<r)return -1;r=min(r,n-r);
+	loop(i,0,r){res*=(n-i);res/=(i+1);}return res;}
+
+int nPr(int n,int r){int res=1;if(n<r)return -1;
+	loop(i,0,r)res*=(n-i);return res;}
+   
+     
+    void solve() {
+     int n,t;
+     cin>>n>>t;
+     string s;
+     cin>>s;
+     string d;
+     d=s;
+     
+    while(t--){ 
+    
+    	for(int i=1;i<n;i++)
+    	{ if(s[i-1]=='B'&&s[i]=='G'){
+    		d[i-1]='G';
+    		d[i]='B';
+	    }
+    	
+    		
+	    }
+	    s=d;
+    }
+    cout<<s<<endl;
+     
+     
+    
+}
+    int main()
+    {
+        fast_io;
+        
+        int T;
+        //cin >> T;
+      T=1;
+        while(T--) {
+            solve();
+        }
+       
+        return 0;
+    }
